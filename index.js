0825621d7f9ccb9d328bd1bf6b8b26c9b0407266
Manojkumar C <73053026+manojkumarcit@users.noreply.github.com>
2024-02-19 22:13:14 +0530

    first commit

--- index.js
+++ index.js
@@ -1,65 +1,60 @@
-function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0,
-            v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
 let todoInput = document.querySelector(".input");
 let addTodoButton = document.querySelector(".button");
 let showTodos = document.querySelector(".todos-container");
-let todo = "";
-
-let localData = JSON.parse(localStorage.getItem("todos"));
+let todo;
+let localData = JSON.parse(localStorage.getItem("todo"))
 let todoList = localData || [];
+
+// creating a function to get unique id
+
+
+function uuid() {
+    return 'xxxxxxx-xxx-4xxx-yxxx-xxxxxxxxxx'.replace(/[xy]/g, function(param) {
+        let number = Math.random() * 16 | 0;
+        let randomNumber = param == 'x' ? number : (number & 0x3 | 0x8);
+        return randomNumber.toString(16);
+    });
+}
 
 addTodoButton.addEventListener("click", (e) => {
     e.preventDefault();
     todo = todoInput.value;
     if (todo.length > 0) {
         todoList.push({
-            todo,
             id: uuid(),
+            todo: todo,
             isCompleted: false
-        });
-        renderTodoList(todoList);
-        localStorage.setItem("todos", JSON.stringify(todoList));
-        todoInput.value = "";
+        })
     }
-});
+
+    renderTodoList(todoList);
+    localStorage.setItem("todo", JSON.stringify(todoList))
+    todoInput.value = "";
+})
 
 showTodos.addEventListener("click", (e) => {
-    e.preventDefault();
     let key = e.target.dataset.key;
-    let delTodoKey = e.target.dataset.todokey;
-    todoList = todoList.map((todo) =>
-        todo.id === key ? {
-            ...todo,
-            isCompleted: !todo.isCompleted
-        } : todo
-    );
-    todoList = todoList.filter((todo) => todo.id !== delTodoKey);
-    localStorage.setItem("todos", JSON.stringify(todoList));
-    console.log(todoList);
-    renderTodoList(todoList);
-});
+    let deltodokey = e.target.dataset.todokey;
+    todoList = todoList.map(todo =>
+        todo.id === key ? {...todo, isCompleted: !todo.isCompleted } : todo)
+    todoList = todoList.filter(todo => todo.id !== deltodokey)
+    localStorage.setItem("todo", JSON.stringify(todoList))
+        // console.log(key + " " + deltodokey)
+    renderTodoList(todoList)
+
+
+})
 
 function renderTodoList(todoList) {
-    showTodos.innerHTML = todoList.map(
-        ({
-            todo,
-            id,
-            isCompleted
-        }) =>
-        `<div class="todo relative"> <input id="item-${id}" data-key=${id} class="t-checkbox t-pointer" type="checkbox" ${
-        isCompleted ? "checked" : ""
-      }> <label data-key=${id} class="todo-text t-pointer ${
-        isCompleted ? "checked-todo" : ""
-      }" for="item-${id}"> ${todo} </label> <button class="absolute right-0 button cursor">
-      <span data-todokey=${id}  class="del-btn material-icons-outlined">delete</span>
-            </button> </div>`
-    );
+
+    showTodos.innerHTML = todoList.map(({ id, todo, isCompleted }) =>
+        `<div class = "relative">
+            <input class = "t-checkbox t-pointer" id = "item-${id}" type = "checkbox" data-key = ${id} ${isCompleted ? "checked": ""}>
+            <label for = "item-${id}"  class ="todo todo-text t-pointer ${isCompleted ? "checked-todo" : ""}" data-key = ${id}>${todo}</label>
+            <button class = "button cursor" data-todokey = ${id}>
+                <span  class = "material-icons-outlined del-btn" data-todokey = ${id}>delete</span>
+            </button>
+        </div>`).join("")
 }
 
 renderTodoList(todoList);
